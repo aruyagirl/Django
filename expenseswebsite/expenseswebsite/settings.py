8e4dfd4fcc0c7a73f107c5dc7d8b37fc040d819a
aruyagirl <baney.zhao@gmail.com>
2024-04-21 11:29:05 +1000

    add

--- expenseswebsite/expenseswebsite/settings.py
+++ expenseswebsite/expenseswebsite/settings.py
@@ -12,10 +12,13 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
+from dotenv import load_dotenv
 import environ
 
 env = environ.Env()
 environ.Env.read_env(env_file='.env')
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
