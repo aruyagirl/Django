8538997ce11158e31cae10a9441879cf719847ca
aruyagirl <baney.zhao@gmail.com>
2024-04-21 15:00:07 +1000

    add

--- expenseswebsite/build_files.sh
+++ expenseswebsite/build_files.sh
@@ -1,4 +1,4 @@
-python 3 -m pip install -r requirements.txt
-python 3 manage.py makemigrations --noinput
-python 3 manage.py migrate --noinput
+python3 -m pip install -r requirements.txt
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
 python3 manage.py collectstatic --noinput
